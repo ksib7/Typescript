f7dd21c3a453bdfac110ee6de7e7efa33636bcd0
ksib7 <sibirkin70@gmail.com>
2022-10-12 23:14:12 +0300

    homework_3

---
+++ src/user/user.ts
@@ -0,0 +1,27 @@
+import { renderBlock } from "../helpers/renderBlock.js";
+
+export function renderUserBlock(
+  username: string,
+  avatarSource: string,
+  favoriteItemsAmount?: number
+) {
+  const favoritesCaption = favoriteItemsAmount || "ничего нет";
+  const hasFavoriteItems = !!favoriteItemsAmount;
+
+  renderBlock(
+    "user-block",
+    `
+    <div class="header-container">
+      <img class="avatar" src=${avatarSource} alt="Wade Warren" />
+      <div class="info">
+          <p class="name">${username}</p>
+          <p class="fav">
+            <i class="heart-icon${
+              hasFavoriteItems ? " active" : ""
+            }"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  );
+}
