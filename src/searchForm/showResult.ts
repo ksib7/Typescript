29e26844f0091cd510542cd698d36f853193ce07
ksib7 <sibirkin70@gmail.com>
2022-10-23 11:54:51 +0300

    homework_4

--- src/searchForm/showResult.ts
+++ src/searchForm/showResult.ts
@@ -1,5 +1,7 @@
 import { SearchFormData } from "./searchFormTypes";
 
-export const showResult = (values: SearchFormData) => {
-  console.log(values);
-};
+export const showResult = (values: SearchFormData) => ({
+  arrival: new Date(values.arrival),
+  departure: new Date(values.departure),
+  maxPrice: Number(values.maxPrice),
+});
